658d534dbcbc89728663ed69ddf283dca983851a
m0wh <malo.dewiderspach@gmail.com>
2021-01-12 01:32:12 +0100

    add diffenrent data types

--- src/main.ts
+++ src/main.ts
@@ -22,17 +22,35 @@ camera.position.z = CONFIG.n * -4
 
 const { composer } = fx({ renderer, scene, camera })
 
-const minMax = [9999999, 0]
+let data = 'taken'
 
-let values = new Array(CONFIG.n * 2).fill(new Array(CONFIG.n * 2).fill(0))
+const minMax = {
+  taken: [9999999, 0],
+  free: [9999999, 0],
+  electric: [9999999, 0]
+}
+
+const hueMinMax = {
+  taken: [200, 230],
+  free: [350, 375],
+  electric: [120, 150]
+}
+
+document.querySelectorAll('.change').forEach(btn => {
+  btn.addEventListener('click', () => {
+    data = btn.getAttribute('data')
+  })
+})
+
+let values = new Array(CONFIG.n * 2).fill(new Array(CONFIG.n * 2).fill({ taken: 0, free: 0 }))
 const bars = values.map((row, x) => row.map((val, y) => {
   const geo = new BoxBufferGeometry(CONFIG.siz, 1, CONFIG.siz)
   const mat = new MeshPhongMaterial({ color: 0xffffff })
-  const bar = new Mesh(geo, mat)
-  bar.position.set(x - CONFIG.n, (CONFIG.scale * val) / 2, y - CONFIG.n)
-  bar.scale.setY(CONFIG.scale * val)
-  bar.visible = val > 0
-  return { mesh: bar, geo, mat }
+  const mesh = new Mesh(geo, mat)
+  mesh.position.set(x - CONFIG.n, (CONFIG.scale * val.taken) / 2, y - CONFIG.n)
+  mesh.scale.setY(CONFIG.scale * val.taken)
+  mesh.visible = val.taken > 0
+  return { mesh, geo, mat }
 }))
 
 bars.forEach(row => row.forEach(bar => scene.add(bar.mesh)))
@@ -51,23 +69,37 @@ function getData () {
       for (let x = -CONFIG.n; x < CONFIG.n; x++) {
         const row = []
         for (let y = -CONFIG.n; y < CONFIG.n; y++) {
-          const velibsInTile = velib.records.filter(station => {
+          const inTile = velib.records.filter(station => {
             const isInX = x === Math.floor(range(station.geometry.coordinates[0], 2.485008, 2.204614, -CONFIG.n, CONFIG.n))
             const isInY = y === Math.floor(range(station.geometry.coordinates[1], 48.767656, 48.955623, -CONFIG.n, CONFIG.n))
             return isInX && isInY
-          }).reduce((acc, val) => acc + val.fields.numbikesavailable, 0)
+          })
 
-          if (velibsInTile > minMax[1]) minMax[1] = velibsInTile
-          if (velibsInTile < minMax[0]) minMax[0] = velibsInTile
+          const velibsInTile = inTile.reduce((acc, val) => acc + val.fields.numbikesavailable, 0)
+          const freeInTile = inTile.reduce((acc, val) => acc + (val.fields.capacity - val.fields.numbikesavailable), 0)
+          const electricInTile = inTile.reduce((acc, val) => acc + val.fields.ebike, 0)
 
-          row.push(velibsInTile)
+          if (velibsInTile > minMax.taken[1]) minMax.taken[1] = velibsInTile
+          if (velibsInTile < minMax.taken[0]) minMax.taken[0] = velibsInTile
+
+          if (freeInTile > minMax.free[1]) minMax.free[1] = freeInTile
+          if (freeInTile < minMax.free[0]) minMax.free[0] = freeInTile
+
+          if (electricInTile > minMax.electric[1]) minMax.electric[1] = electricInTile
+          if (electricInTile < minMax.electric[0]) minMax.electric[0] = electricInTile
+
+          row.push({
+            taken: velibsInTile,
+            free: freeInTile,
+            electric: electricInTile
+          })
         }
         grided.push(row)
       }
 
       values = grided
 
-      setSpanText('total', grided.reduce((acc, val) => acc + val.reduce((acc2, val2) => acc2 + val2, 0), 0))
+      setSpanText('total', grided.reduce((acc, val) => acc + val.reduce((acc2, val2) => acc2 + val2.taken, 0), 0))
       const biggest = velib.records.sort((a, b) => b.fields.numbikesavailable - a.fields.numbikesavailable)[0]
       setSpanText('stat', biggest.fields.name)
       setSpanText('maxstatvelibs', biggest.fields.numbikesavailable)
@@ -89,10 +121,7 @@ setInterval(getData, 60000)
 
 const texture = new TextureLoader().load(parismap)
 const planeGeo = new PlaneBufferGeometry(CONFIG.n * 2, CONFIG.n * 2, 1)
-const planeMat = new MeshPhongMaterial({
-  map: texture,
-  side: DoubleSide
-})
+const planeMat = new MeshPhongMaterial({ map: texture, side: DoubleSide })
 const plane = new Mesh(planeGeo, planeMat)
 plane.position.y = -0.01
 plane.rotation.x = Math.PI / 2
@@ -108,25 +137,28 @@ scene.add(sun)
 scene.add(sun.target)
 scene.add(ambient)
 
-raf.subscribe((time) => {
-  composer.render()
-
+raf.subscribe(() => {
   values.forEach((row, x) => {
     row.forEach((tile, y) => {
+      const value = tile[data]
+      const rng = minMax[data]
+
       bars[x][y].mesh.position.set(
         lerp(bars[x][y].mesh.position.x, x - CONFIG.n, 0.1),
-        lerp(bars[x][y].mesh.position.y, (CONFIG.scale * tile) / 2, 0.1),
+        lerp(bars[x][y].mesh.position.y, (CONFIG.scale * value) / 2, 0.1),
         lerp(bars[x][y].mesh.position.z, y - CONFIG.n, 0.1)
       )
-      bars[x][y].mesh.scale.y = lerp(bars[x][y].mesh.scale.y, CONFIG.scale * tile, 0.1)
-      bars[x][y].mesh.visible = tile > 0
+      bars[x][y].mesh.scale.y = lerp(bars[x][y].mesh.scale.y, CONFIG.scale * value, 0.1)
+      bars[x][y].mesh.visible = value > 0
 
-      const hue = Math.round(range(tile, minMax[0], minMax[1], 200, 230)) % 360
-      const sat = Math.round(range(tile, minMax[0], minMax[1], 50, 100))
-      const lum = Math.round(range(tile, minMax[0], minMax[1], 80, 40))
+      const hue = Math.round(range(value, rng[0], rng[1], hueMinMax[data][0], hueMinMax[data][1])) % 360
+      const sat = Math.round(range(value, rng[0], rng[1], 50, 100))
+      const lum = Math.round(range(value, rng[0], rng[1], 80, 40))
       bars[x][y].mat.color = new Color(`hsl(${hue}, ${sat}%, ${lum}%)`)
     })
   })
+
+  composer.render()
 })
 
 function setSpanText (id, text: string) {
